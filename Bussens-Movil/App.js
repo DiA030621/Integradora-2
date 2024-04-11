9642bba664649a0b5277a03a24d47419ef696e94
Dev Jack <poncealucard@gmail.com>
2024-04-11 14:15:21 -0600

    Se envían datos de gps a firebase

--- Bussens-Movil/App.js
+++ Bussens-Movil/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import Navigation from './Navigation';
+import { readDataRealTime } from './screens/firebase'; // Asegúrate de ajustar la ruta de importación según sea necesario
 
 export default function App() {
-  return (
+ useEffect(() => {
+    // Llama a la función readDataRealTime cuando el componente se monte
+    readDataRealTime();
+ }, []); // El array vacío como segundo argumento asegura que el efecto se ejecute solo una vez, al montar el componente
+
+ return (
     <Navigation/>
-  );
+ );
 }
