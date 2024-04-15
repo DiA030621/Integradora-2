3c590a53d7d942cb18096adeb5ce9fa122ef9c95
YiyoMb <diegobalderas.864eyou@gmail.com>
2024-04-14 19:44:16 -0600

    Se agrega gráfica sin datos funcionales

--- Bussens-Movil/screens/frecuenteScreen.js
+++ Bussens-Movil/screens/frecuenteScreen.js
@@ -1,16 +1,61 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { PieChart } from 'react-native-chart-kit';
 
 const MainScreen = () => {
-  const route = useRoute();
-  const { clave } = route.params;
+  // Datos de ejemplo para el gráfico de pastel
+  const data = [
+    {
+      name: 'Ruta A',
+      population: 100,
+      color: '#FF6347',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Ruta B',
+      population: 200,
+      color: '#4682B4',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Ruta C',
+      population: 300,
+      color: '#32CD32',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text style={styles.message}>¡Agrega rutas a frecuentes para visualizarlas aquí!</Text>
+      <Text style={styles.message}>Estas son las rutas que más frecuentas</Text>
+      <PieChart
+        data={data}
+        width={300}
+        height={200}
+        chartConfig={{
+          backgroundColor: '#ffffff',
+          backgroundGradientFrom: '#ffffff',
+          backgroundGradientTo: '#ffffff',
+          decimalPlaces: 0,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute
+      />
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
